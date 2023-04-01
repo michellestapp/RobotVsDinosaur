d33cb45898698346a30f704dd11e9c311c45b0f2
michellestapp <mstapp@rocketmail.com>
2023-03-31 19:31:49 -0500

    Added battle_phase with attack working

--- dinosaur.py
+++ dinosaur.py
@@ -1,8 +1,12 @@
 class Dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
-        self.attack_power = 20
+        self.attack_power = attack_power
         self.health = 100
 
     def attack(self, robot):
-        print(" Dino attacks Robot")
+        print(f" Dino {self.name} attacked Robot {robot.name}")
+        robot.health -= self.attack_power
+        print(f" Robot {robot.name}'s health score is nnow {robot.health}")
+        print(robot)
+        
